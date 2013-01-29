a122cf0f20078881a2283c1c817b3261ca1c1a95
Jim Jones <cnamejj@gmail.com>
2013-01-29 00:19:34 -0800

    Include GPL info in main library

--- ProcSysInfo.py
+++ ProcSysInfo.py
@@ -4,6 +4,12 @@
 Describe what's in this module and put that info here...
 """
 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version
+# 2 of the License, or (at your option) any later version.
+
+
 import time
 import socket
 from subprocess import Popen, PIPE
