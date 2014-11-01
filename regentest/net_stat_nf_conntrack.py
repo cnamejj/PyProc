bd0f810460513b3921abe9d39e3bcf943f459190
Jim Jones <cnamejj@gmail.com>
2014-11-01 15:27:49 -0700

    Add the last 3 recreators for /proc/net/stat/* files
    Datafiles handled by the code in this commit are:
    /proc/net/stat/ndisc_cache, /proc/net/stat/nf_conntrack.py,
    and /proc/net/stat/rt_cache.

---
+++ regentest/net_stat_nf_conntrack.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""Handle records from /proc/net/stat/nf_conntrack data files"""
+
+import regentest as RG
+import ProcHandlers as PH
+
+PFC = PH.ProcFieldConstants
+
+# ---
+
+def re_net_stat_nf_conntrack(inprecs):
+
+    """Iterate through parsed records and re-generate data file"""
+
+    __head = "entries  searched found new invalid ignore delete delete_list \
+insert insert_failed drop early_drop icmp_error  expect_new expect_create \
+expect_delete search_restart"
+
+    __template = "{ents:08x}  {search:08x} {found:08x} {new:08x} {inv:08x} \
+{ign:08x} {delete:08x} {dlist:08x} {ins:08x} {ifail:08x} {drop:08x} \
+{dearly:08x} {icmp:08x}  {exnew:08x} {excr:08x} {exdel:08x} {srest:08x}"
+
+    print __head
+
+    for __hilit in inprecs:
+        __ff = inprecs.field
+
+        print __template.format(ents=__ff[PFC.F_ENTRIES], 
+                search=__ff[PFC.F_SEARCHED], found=__ff[PFC.F_FOUND],
+                new=__ff[PFC.F_NEW], inv=__ff[PFC.F_INVALID],
+                ign=__ff[PFC.F_IGNORE], delete=__ff[PFC.F_DELETE],
+                dlist=__ff[PFC.F_DELETE_LIST], ins=__ff[PFC.F_INSERT],
+                ifail=__ff[PFC.F_INSERT_FAILED], drop=__ff[PFC.F_DROP],
+                dearly=__ff[PFC.F_DROP_EARLY], icmp=__ff[PFC.F_ICMP_ERROR],
+                exnew=__ff[PFC.F_EXP_NEW], excr=__ff[PFC.F_EXP_CREATE],
+                exdel=__ff[PFC.F_EXP_DELETE], srest=__ff[PFC.F_SEARCH_RESTART])
+
+#...+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8
+
+RG.RECREATOR[PH.GET_HANDLER("/proc/net/stat/nf_conntrack")] = \
+        re_net_stat_nf_conntrack
+
+    
+
---
+++ regentest/net_stat_rt_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""Handle records from /proc/net/stat/rt_cache data files"""
+
+import regentest as RG
+import ProcHandlers as PH
+
+PFC = PH.ProcFieldConstants
+
+# ---
+
+def re_net_stat_rt_cache(inprecs):
+
+    """Iterate through parsed records and re-generate data file"""
+
+    __head = "entries  in_hit in_slow_tot in_slow_mc in_no_route in_brd \
+in_martian_dst in_martian_src  out_hit out_slow_tot out_slow_mc  gc_total \
+gc_ignored gc_goal_miss gc_dst_overflow in_hlist_search out_hlist_search"
+
+    __template = "{ents:08x}  {ihit:08x} {istot:08x} {ismc:08x} {inor:08x} \
+{ibrd:08x} {imdst:08x} {imsrc:08x}  {ohit:08x} {ostot:08x} {osmc:08x} \
+{gctot:08x} {gcig:08x} {gcgm:08x} {gcdo:08x} {ihls:08x} {ohls:08x} "
+
+    print __head
+
+    for __hilit in inprecs:
+        __ff = inprecs.field
+
+        print __template.format(ents=__ff[PFC.F_ENTRIES],
+                ihit=__ff[PFC.F_IN_HIT], istot=__ff[PFC.F_IN_SLOW_TOT],
+                ismc=__ff[PFC.F_IN_SLOW_MC], ibrd=__ff[PFC.F_IN_BRD],
+                imdst=__ff[PFC.F_IN_MARTIAN_DST], 
+                imsrc=__ff[PFC.F_IN_MARTIAN_SRC], inor=__ff[PFC.F_IN_NO_ROUTE],
+                ohit=__ff[PFC.F_OUT_HIT], ostot=__ff[PFC.F_OUT_SLOW_TOT],
+                osmc=__ff[PFC.F_OUT_SLOW_MC], gctot=__ff[PFC.F_GC_TOTAL],
+                gcig=__ff[PFC.F_GC_IGNORED], gcgm=__ff[PFC.F_GC_GOAL_MISS],
+                gcdo=__ff[PFC.F_GC_DST_OVERFLOW], ihls=__ff[PFC.F_IN_HL_SEARCH],
+                ohls=__ff[PFC.F_OUT_HL_SEARCH])
+
+#...+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8
+
+RG.RECREATOR[PH.GET_HANDLER("/proc/net/stat/rt_cache")] = re_net_stat_rt_cache
+
+    
+
