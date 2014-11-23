f59437d02cc44d60a6dcf0f916754c6dbf4afc34
Jim Jones <cnamejj@gmail.com>
2014-11-22 21:23:25 -0800

    Bug fix, "mode" field in /proc/sysvipc/shm records is octal, not base 10

--- ProcSysvipcHandlers.py
+++ ProcSysvipcHandlers.py
@@ -97,7 +97,7 @@ class ProcSysvipcSHM(PBR.FixedWhitespaceDelimRecs):
 
         PBR.add_parse_rule(self, { POS: 0, NAME: PFC.F_KEY, CONV: long } )
         PBR.add_parse_rule(self, { POS: 1, NAME: PFC.F_ID, CONV: long } )
-        PBR.add_parse_rule(self, { POS: 2, NAME: PFC.F_MODE, CONV: long } )
+        PBR.add_parse_rule(self, { POS: 2, NAME: PFC.F_MODE, CONV: long, BASE: 8 } )
         PBR.add_parse_rule(self, { POS: 3, NAME: PFC.F_SIZE, CONV: long } )
         PBR.add_parse_rule(self, { POS: 4, NAME: PFC.F_CPID, CONV: long } )
         PBR.add_parse_rule(self, { POS: 5, NAME: PFC.F_LPID, CONV: long } )
