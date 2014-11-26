0fff7e52c2c1c3310f0505e737b9d3244a111678
Jim Jones <cnamejj@gmail.com>
2014-11-26 01:49:12 -0800

    Recreator for /proc/crypto files

--- regentest/__init__.py
+++ regentest/__init__.py
@@ -38,7 +38,7 @@ __all__ = ["net_tcp", "net_udp", "net_tcp6", "net_udp6", "net_unix",
     "self_io", "net_ip6_tables_targets", "net_ip6_tables_matches",
     "net_ip6_tables_names", "net_ip_tables_targets", "net_ip_tables_matches",
     "net_ip_tables_names", "self_sched", "net_snmp", "net_netstat",
-    "net_fib_trie"]
+    "net_fib_trie", "root_crypto"]
 
 #...+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8
 
---
+++ regentest/root_crypto.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+"""Handle records from /proc/crypto files"""
+
+import regentest as RG
+import ProcHandlers as PH
+import ProcBaseRoutines as PBR
+
+PREFIX = PBR.PREFIX_VAL
+NAME = PBR.FIELD_NAME
+CONV = PBR.CONVERSION
+
+# ---
+
+def re_root_crypto(inprecs):
+
+    """Iterate through parsed records and re-generate data file"""
+
+    __strtemp = "{pad:<12s} : {val:s}"
+    __longtemp = "{pad:<12s} : {val:d}"
+
+    __template = { str: __strtemp, long: __longtemp }
+
+    for __hilit in inprecs:
+        __ff = inprecs.field
+        __rulelist = inprecs.parse_rule
+        __hitlist = inprecs.hit_order
+
+        __prefix = dict()
+        __conv = dict()    
+
+        for __seq in range(0, len(__rulelist)):
+            __rule = __rulelist[__seq][0]
+
+            try:
+                __prefix[__rule[NAME]] = __rule[PREFIX]
+                try:
+                    __conv[__rule[NAME]] = __rule[CONV]
+                except KeyError:
+                    __conv[__rule[NAME]] = str
+            except KeyError:
+                pass
+
+        for __seq in range(0, len(__hitlist)):
+            __name = __hitlist[__seq]
+            __vt = __conv[__name]
+
+            print __template[__vt].format(pad=__prefix[__name], val=__ff[__name])
+
+        print ""
+
+#...+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8
+
+RG.RECREATOR[PH.GET_HANDLER("/proc/crypto")] = re_root_crypto
