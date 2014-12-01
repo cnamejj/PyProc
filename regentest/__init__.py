6497d508497f44c69173631e7cd6fe7ca2c799d0
Jim Jones <cnamejj@gmail.com>
2014-11-30 22:46:44 -0800

    Recreator for /proc/latency_stats data files

--- regentest/__init__.py
+++ regentest/__init__.py
@@ -39,7 +39,7 @@ __all__ = ["net_tcp", "net_udp", "net_tcp6", "net_udp6", "net_unix",
     "net_ip6_tables_names", "net_ip_tables_targets", "net_ip_tables_matches",
     "net_ip_tables_names", "self_sched", "net_snmp", "net_netstat",
     "net_fib_trie", "root_crypto", "self_environ", "root_slabinfo",
-    "root_version"]
+    "root_version", "root_latency_stats"]
 
 #...+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8
 
---
+++ regentest/root_latency_stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""Handle records from /proc/latency_stats files"""
+
+import regentest as RG
+import ProcHandlers as PH
+
+PFC = PH.PFC 
+
+# ---
+
+def re_root_latency_stats(inprecs):
+
+    """Iterate through parsed records and re-generate data file"""
+
+    __header = "Latency Top version : v0.1"
+
+    __template = "{hits:d} {lacc:d} {lmax:d} {btrace:s}"
+
+    print __header
+
+    for __hilit in inprecs:
+        __ff = inprecs.field
+
+        print __template.format(hits=__ff[PFC.F_HITS],
+                lacc=__ff[PFC.F_ACCUM_LATENCY], lmax=__ff[PFC.F_MAX_LATENCY],
+                btrace=" ".join(__ff[PFC.F_BACKTRACE]))
+
+#...+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8
+
+RG.RECREATOR[PH.GET_HANDLER("/proc/latency_stats")] = re_root_latency_stats
